4b639813bcd1336e5af27180fb1d285c56dd35b3
unknown <shanthanuriyuzaki@gmail.com>
2023-04-28 10:10:55 +0530

    google form linked

--- scripts/footer.js
+++
@@ -1,9 +0,0 @@
-
-function onChange(){
-    $("#submit").attr("disabled",!($('name').val()!="" && $('#query').val()!=""));
-}
-
-function sendEmail() {
-window.open('mailto:shanthanuriyuzaki@gmail.com?subject=Query from '+$('#name').val()+'&body='+$('#query').val());
-}
-
